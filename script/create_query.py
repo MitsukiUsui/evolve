7e0d7ebec58131dd070f17b230d1cfef49e0090b
Mitsuki <mitsuo.com.mu.dummy@gmail.com>
2017-06-09 13:32:31 +0900

    fixed bug of create_query

--- script/create_query.py
+++ script/create_query.py
@@ -9,48 +9,59 @@ def create_query(inFilepath, outFilepath, addDecoy=False):
 			return True
 		else:
 			return False
-	
-	print("\tIN :{}".format(inFilepath))
-	print("\tOUT:{}".format(outFilepath))
-	
-	with open(outFilepath,'w') as f:
-		totalCDS=0
-		processedCDS=0
-		for seq_record in SeqIO.parse(inFilepath, "fasta"):
-			totalCDS+=1
-			if is_typical(seq_record):
-				processedCDS+=1
-				
-				target=[(2,(1,len(seq_record)-2),False),#Frame Number, (start, end), is reverse complement 
-						(3,(2,len(seq_record)-1),False),
-						(4,(0,len(seq_record)),  True),
-						(5,(1,len(seq_record)-2),True),
-						(6,(2,len(seq_record)-1),True)]
 
-				for frameNum, (start,end), revComp in target:
-					seq=seq_record.seq[start:end]
-					if revComp:
-						subSeq=seq.reverse_complement()
-					
-					products=[]
-					headers=[]					  
-					product=str(seq.translate(table=11)).replace('*','X')		  
-					products.append(product)
-					headers.append(">F{0}|{1}".format(frameNum,seq_record.id.strip()))
-					
-					if addDecoy:
-						dr=product[::-1]#decoy reverse
-						products.append(dr)
-						headers.append(">DRF{0}|{1}".format(frameNum,seq_record.id.strip()))
-						ds=''.join(random.sample(product,len(product)))#decoy shuffle
-						products.append(ds)
-						headers.append(">DSF{0}|{1}".format(frameNum,seq_record.id.strip()))
-						
-					for product,header in zip(products, headers):					 
-						f.write(header+'\n')
-						f.write(product+'\n')
-						
-		print("\tDONE: extraction from {0}/{1} CDSs".format(processedCDS, totalCDS))
+	print("\tIN  :{}".format(inFilepath))
+	print("\tOUT :{}".format(outFilepath))
+
+	query_lst=[]
+	decoy_lst=[]
+	
+	totalCDS=0
+	processedCDS=0
+	for seq_record in SeqIO.parse(inFilepath, "fasta"):
+		totalCDS+=1
+		if is_typical(seq_record):
+			processedCDS+=1
+
+			target=[(2,(1,len(seq_record)-2),False),#Frame Number, (start, end), is reverse complement 
+					(3,(2,len(seq_record)-1),False),
+					(4,(0,len(seq_record)),  True),
+					(5,(1,len(seq_record)-2),True),
+					(6,(2,len(seq_record)-1),True)]
+
+			for frameNum, (start,end), revComp in target:
+				seq=seq_record.seq[start:end]
+				if revComp:
+					seq=seq.reverse_complement()
+				
+				queryHeader=">F{0}|{1}".format(frameNum,seq_record.description.strip())
+				querySeq=str(seq.translate(table=11)).replace('*','X')
+				query_lst.append((queryHeader,querySeq))
+				
+				if addDecoy:
+					#decoy reverse
+					drHeader=">DRF{0}|{1}".format(frameNum,seq_record.id.strip())
+					drSeq=querySeq[::-1]
+					decoy_lst.append((drHeader,drSeq))
+				   
+					#decoy shuffle
+					dsHeader=">DSF{0}|{1}".format(frameNum,seq_record.id.strip())
+					dsSeq=''.join(random.sample(querySeq,len(querySeq)))
+					decoy_lst.append((dsHeader,dsSeq))
+
+	#output
+	with open(outFilepath,'w') as f:
+		for header,seq in query_lst:
+			f.write(header+'\n')
+			f.write(seq+'\n')
+		if addDecoy:
+			for header,seq in decoy_lst:
+				f.write(header+'\n')
+				f.write(seq+'\n')
+		
+	
+	print("\tDONE:extraction from {0}/{1} CDSs".format(processedCDS, totalCDS))
+
 		
 def main(targetFilepath):
 	print("START processing {}".format(targetFilepath))
