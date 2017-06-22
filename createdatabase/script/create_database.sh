1aad09584ea9bea1bba667b8f2d0b2fcb6df32e8
Mitsuki <mitsuo.com.mu@gmail.com>
2017-06-22 09:49:09 +0900

    DONE creating database

--- createdatabase/script/create_database.sh
+++ createdatabase/script/create_database.sh
@@ -2,18 +2,21 @@ IFS=$'\n'
 
 for genus in `cat starget_genus.list`
 do
-	outFilepath=/data/mitsuki/data/blast/db/genus_${genus}.fasta
-
-	txtFilepath=../out/genus_${genus}.txt
-	for line in `tail -n +2 ${txtFilepath}`
+	catalogFilepath=../out/genus_${genus}.txt
+	dbDir=/data/mitsuki/data/blast/db
+	fastaFilepath=${dbDir}/genus_${genus}.fasta
+	dbName=${dbDir}/genus_${genus}
+	
+	rm ${fastaFilepath}#!TBI! check existance before
+	echo "CONCAT to ${fastaFilepath}"
+	for line in `tail -n +2 ${catalogFilepath}`
 	do
 		basename=`echo ${line}|cut -d, -f 12`
-		dir=/data/mitsuki/data/refseq
-		filepath=${dir}/protein/${basename}_protein.faa
-		cat ${filepath}>>outFilepath
+		filepath=/data/mitsuki/data/refseq/protein/${basename}_protein.faa
+		echo -e "\t${filepath}"
+		cat ${filepath} >> ${fastaFilepath}
 	done
 
-	makeblastdb -in ${outFilepath} -dbtype prot -out genus_${genus}
-	#rm ${outFilepath}
-	echo "DONE ${outFilepath}."
+	makeblastdb -in ${fastaFilepath} -dbtype prot -out ${dbName}
+	echo "CREATED ${dbName}"
 done
