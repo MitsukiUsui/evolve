6becc60540ed0f7728097800d248e72e133680ac
Mitsuki <mitsuo.com.mu@gmail.com>
2017-06-23 20:02:05 +0900

    test mmseqs function

---
+++ execute/test/convert.sh
@@ -0,0 +1,4 @@
+mmseqs convertalis /data/mitsuki/data/mmseqs/query/GCF_000010665.1_ASM1066v1\
+               		/data/mitsuki/data/mmseqs/db/genus_872\
+               		genus_872\
+               		genus_872.m8 --threads 8 --format-mode 1
---
+++ execute/test/mmseqs.sh
@@ -0,0 +1,4 @@
+mmseqs search /data/mitsuki/data/mmseqs/query/GCF_000010665.1_ASM1066v1\
+               /data/mitsuki/data/mmseqs/db/genus_872\
+               genus_872\
+               /tmp/mitsuki -a -e 1 --threads 8 --remove-tmp-files 
